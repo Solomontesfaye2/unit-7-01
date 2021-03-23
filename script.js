a7a4cf80cc26ff36224d7c84b0e45ae55fdbe995
Solomon Tesfaye <solomon.tesfaye@ocsbstudent.ca>
2021-03-23 17:31:47 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('button').addEventListener('click', randomizer)
+let randomNumber = 0
+let userGuess = 0
+ 
+function randomizer () {
+userGuess= document.getElementById('input').value
+ userGuess= parseInt(userGuess)
+ 
+ randomNumber = (Math.random() * 6) + 5
+ randomNumber = parseInt(randomNumber)
+ 
+ if (userGuess == randomNumber) {
+alert('Congratulation You Win !!!')
+ }
+}
+ 
